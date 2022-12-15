ca15ea68f04cc03668b98dc9481d535f1cac7386
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-12-15 23:30:15 +0900

    Nest Kickoff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { QatarworldcupService } from './qatar-world-cup/qatar-world-cup.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QatarWorldCupController } from './qatar-world-cup/quatar-world-cup.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, QatarWorldCupController],
+  providers: [AppService, QatarworldcupService],
 })
 export class AppModule {}
